4f9fd733fe72db4456a042938ffa751c854df2d8
Gabriel Jara <149518249+gao4323@users.noreply.github.com>
2026-08-28 23:29:23 +0000

    Carga Inicial

---
+++ demospringrest/src/main/java/edu/pe/uls/demospringrest/DemospringrestApplication.java
@@ -0,0 +1,13 @@
+package edu.pe.uls.demospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemospringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemospringrestApplication.class, args);
+	}
+
+}
